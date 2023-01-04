f75041f52f5ff24afcb188897ef81a25aa118406
testrungirl <laigbodioh@gmail.com>
2023-01-04 13:46:59 +0100

    Testing Arrays and collection

--- TestNinja.NUnitTests/MathTests.cs
+++ TestNinja.NUnitTests/MathTests.cs
@@ -12,21 +12,20 @@
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var result = _math.Add(1,2);
+            var result = _math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
         }
         [Test]
-        [TestCase(2,1,2)]
-        [TestCase(1,2,2)]
-        [TestCase(2,2,2)]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(2, 2, 2)]
         public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
         {
             var result = _math.Max(a, b);
 
             Assert.That(result, Is.EqualTo(expectedResult));
-        } 
-        
+        }
         [Test]
         public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
         {
@@ -34,5 +33,6 @@
 
             Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5 }));
         }
+
     }
 }
