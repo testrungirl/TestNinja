ff8a5b339c3992677046cea6c60ff822b5c044f8
testrungirl <laigbodioh@gmail.com>
2023-01-04 14:27:37 +0100

    Testing the return types of methods

---
+++ TestNinja.NUnitTests/CustomerControllerTests.cs
@@ -0,0 +1,39 @@
+﻿
+using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new CustomerController();
+        }
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var result = _controller.GetCustomer(0);
+            //NotFound
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //NotFound or one of its derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnOk()
+        {
+            var result = _controller.GetCustomer(5);
+            //NotFound
+
+            Assert.That(result, Is.TypeOf<Ok>());
+
+            //NotFound or one of its derivatives
+            //Assert.That(result, Is.InstanceOf<Ok>());
+        }
+    }
+}
