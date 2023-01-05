6e381f5ef0850cec7ead53fcd618163e00109215
testrungirl <laigbodioh@gmail.com>
2023-01-05 06:06:51 +0100

    Testing events that throw exceptions

---
+++ TestNinja.NUnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,22 @@
+﻿
+using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        public ErrorLogger _errorLogger;
+        [SetUp]
+        public void SetUp() {
+            _errorLogger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            _errorLogger.Log("a");
+            Assert.That(_errorLogger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
