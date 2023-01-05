0a156678c7d16ee9fe2f20e89a69088a5bb67cbb
testrungirl <laigbodioh@gmail.com>
2023-01-05 07:04:54 +0100

    FizzBuzz Tests

---
+++ TestNinja.NUnitTests/FizzBuzzTests.cs
@@ -0,0 +1,21 @@
+﻿using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {       
+        [Test]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(2, "2")]
+        public void GetOutput_WhenCalled_ReturnsAString(int a, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(a);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+    }
+}
