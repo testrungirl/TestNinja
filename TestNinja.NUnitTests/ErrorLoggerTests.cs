fd6b0cf84564befd4454149ea13a81f42ef2c997
testrungirl <laigbodioh@gmail.com>
2023-01-05 06:27:49 +0100

    Test for null exception

--- TestNinja.NUnitTests/ErrorLoggerTests.cs
+++ TestNinja.NUnitTests/ErrorLoggerTests.cs
@@ -18,5 +18,15 @@ namespace TestNinja.NUnitTests
             _errorLogger.Log("a");
             Assert.That(_errorLogger.LastError, Is.EqualTo("a"));
         }
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("  ")]
+        public void Log_InvalidError_throwArgumnetNullException(string error)
+        {
+            Assert.That(() => _errorLogger.Log(error), Throws.ArgumentNullException);
+            
+            //Assert.That(() => _errorLogger.Log(error), Throws.Exception.TypeOf<DivideByZeroException>);
+        }
     }
 }
